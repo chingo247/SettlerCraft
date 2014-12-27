37710c7b7842f5610fcfdad0e6339168d746fe01
Chingo247 <Chingo@live.nl>
2014-12-27 15:49:55 +0100

    Added 'Core' classes

---
+++ SettlerCraftCore/src/main/java/com/chingo247/xcore/core/IPlayer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chingo247.xcore.core;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author Chingo
+ */
+public interface IPlayer {
+    
+    public String getName();
+    
+    public UUID getUniqueId();
+    
+    public IWorld getWorld();
+    
+    public void sendMessage(String message);
+    
+    public void sendMessage(String... message);
+    
+    public float getYaw();
+    
+    public int getLevel();
+    
+    public int getExperience();
+    
+}
---
+++ SettlerCraftCore/src/main/java/com/chingo247/xcore/util/ChatColors.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2014 Chingo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.chingo247.xcore.util;
+
+/**
+ *
+ * @author Chingo
+ */
+public class ChatColors {
+
+    /**
+     * Automatically generated
+     */
+    public static final String BLACK = "ǰ";
+    public static final String DARK_BLUE = "Ǳ";
+    public static final String DARK_GREEN = "ǲ";
+    public static final String DARK_AQUA = "ǳ";
+    public static final String DARK_RED = "Ǵ";
+    public static final String DARK_PURPLE = "ǵ";
+    public static final String GOLD = "Ƕ";
+    public static final String GRAY = "Ƿ";
+    public static final String DARK_GRAY = "Ǹ";
+    public static final String BLUE = "ǹ";
+    public static final String GREEN = "ȡ";
+    public static final String AQUA = "Ȣ";
+    public static final String RED = "ȣ";
+    public static final String LIGHT_PURPLE = "Ȥ";
+    public static final String YELLOW = "ȥ";
+    public static final String WHITE = "Ȧ";
+    public static final String MAGIC = "ȫ";
+    public static final String BOLD = "Ȭ";
+    public static final String STRIKETHROUGH = "ȭ";
+    public static final String UNDERLINE = "Ȯ";
+    public static final String ITALIC = "ȯ";
+    public static final String RESET = "Ȳ";
+
+}
